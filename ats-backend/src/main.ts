5138c26deadae5aaf9eaf891f2985ee44b0fa24a
Premiit <prem21pcp@gmail.com>
2025-05-07 21:36:44 +0530

    speed

--- ats-backend/src/main.ts
+++ ats-backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Allow sending cookies or authentication headers
   });
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
